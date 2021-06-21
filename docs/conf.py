754f1dc80b4e40a6ad2a17570c61f663c2008075
kevin@inrosoftware.com <kevin@inrsoftware.com>
2021-06-21 10:11:00 -0400

    Black reformatting of base setup and config and template tests/test_tests.py

--- docs/conf.py
+++ docs/conf.py
@@ -26,9 +26,9 @@ import tm2py
 
 # -- Project information -----------------------------------------------------
 
-project = 'tm2py'
-copyright = '2021, Bay Area Metro'
-authors = 'TBD'
+project = "tm2py"
+copyright = "2021, Bay Area Metro"
+authors = "TBD"
 version = tm2py.__version__
 
 # -- General configuration ---------------------------------------------------
@@ -52,12 +52,12 @@ extensions = [
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -70,10 +70,9 @@ html_theme = "sphinx_rtd_theme"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
-intersphinx_mapping = {
-}
+intersphinx_mapping = {}
 
 autodoc_default_options = {
     "members": True,
--- tests/test_tests.py
+++ tests/test_tests.py
@@ -1,14 +1,13 @@
 import pytest
 
+
 @pytest.mark.skipci
 def test_skipci():
-    """Shouldn't be run on CI server.
-    """
+    """Shouldn't be run on CI server."""
     print("If this is a CI server, the marker isn't working!!!")
 
 
 def test_testing():
-    """Tests that tests are run.
-    """
+    """Tests that tests are run."""
     print("Tests are being run!")
-    import tm2py 
+    import tm2py
