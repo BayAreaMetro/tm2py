986e3d9ef9efbe773945b565b835920da504c9cc
kevin@inrosoftware.com <kevin@inrsoftware.com>
2021-07-16 16:31:20 -0400

    First working version of airpassenger model, interpolated demand based on linear growth 2007-2035, fixed scaling calculation.

--- tm2py/model/demand/airpassenger.py
+++ tm2py/model/demand/airpassenger.py
@@ -1,37 +1,55 @@
-"""TODO: review module docsting 
-Build the airport trip matrices (these are independent of level-of-service)
-    Run the airport model
-        "nonres/BuildAirPax.job"
-            input: nonres/@token_year@_@token_toFrom@@token_airport@.csv
-            output: five time-of-day-specific tables for DA, SR2, SR3, DATOLL, SR2TOLL, SR3TOLL
-            Looks like a pandas application
+"""Build the airport trip matrices.
 
-NOTES FROM BuildAirPaxMatrices.job
+    input: nonres/{year}_{tofrom}{airport}.csv
+    output: five time-of-day-specific OMX files with matrices DA, SR2, SR3
 
- TP+ script that creates air passenger vehicle trip tables for the Bay Area's three major airports, namely SFO,
- OAK, and SJC.  Geoff Gosling, a consultant, created vehicle trip tables segmented by time of day, travel mode,
- and access/egress direction (i.e. to the airport or from the airport) for years 2007 and 2035.  The tables are
- based on a 2006 Air Passenger survey, which was conducted at SFO and OAK (but not SJC).  The five time
- periods correspond to the time periods used by the travel model and are as follows: (a) early AM, before 6 am; 
- (b) AM peak period, 6 am to 10 am; (c) midday, 10 am to 3 pm; (d) PM peak period, 3 pm to 7 pm; and, (e) evening, 
- after 7 pm.  The travel modes are as follows: (a) escort (drive alone, shared ride 2, and shared ride 3+); (b) 
- park (da, sr2, & sr3+); (c) rental car (da, sr2, & sr3+); (d) taxi ((da, sr2, & sr3+); (e) limo (da, sr2, & sr3+);
- (f) shared ride van (all assumed to be sr3); (g) hotel shuttle (all assumed to be sr3); and, (h) charter bus (all
- assumed to be sr3).  The shared ride van, hotel shuttle, and charter bus modes are assumed to have no deadhead travel. 
- The return escort trip is included, as are the deadhead limo and taxi trips. 
+NOTES:
 
- The scripts reads in csv files adapted from Mr. Gosling's Excel files, then creates a Cube matrix for each of the
- file.  Next, a highway-assignment ready matrix file is creating for each time-of-day interval.  Here, we 
- assume that no air passengers use HOT lanes (probably not exactly true in certain future year scenarios, but the 
- assumption is made here as a simplification).  Simple linear interpolations are used to estimate vehicle demand in
- years other than 2007 and 2035, including 2015, 2020, 2025, 2030, and 2040.  The 2007 table is used for years 2000,
- 2005, and 2010.
+These are independent of level-of-service).
 
- Transit travel to the airports is not included in these vehicle trip tables.
+Creates air passenger vehicle trip tables for the Bay Area's three major
+airports, namely SFO, OAK, and SJC.  Geoff Gosling, a consultant, created
+vehicle trip tables segmented by time of day, travel mode, and access/egress
+direction (i.e. to the airport or from the airport) for years 2007 and 2035.
+The tables are based on a 2006 Air Passenger survey, which was conducted
+at SFO and OAK (but not SJC).  The five time periods correspond to the time
+periods used by the travel model and are as follows:
+    (a) early AM, before 6 am
+    (b) AM peak period, 6 am to 10 am
+    (c) midday, 10 am to 3 pm
+    (d) PM peak period, 3 pm to 7 pm
+    (e) evening,  after 7 pm
 
+The travel modes are as follows:
+    (a) escort (drive alone, shared ride 2, and shared ride 3+)
+    (b) park (da, sr2, & sr3+)
+    (c) rental car (da, sr2, & sr3+)
+    (d) taxi ((da, sr2, & sr3+)
+    (e) limo (da, sr2, & sr3+)
+    (f) shared ride van (all assumed to be sr3);
+    (g) hotel shuttle (all assumed to be sr3); and,
+    (h) charter bus (all assumed to be sr3).
 
- Input:  (A)  Year-, access/egress-, and airport-specific database file with 90 columns of data for each TAZ.  There
-              are 18 columns for each time-of-day interval as follows:
+The shared ride van, hotel shuttle, and charter bus modes are assumed to
+have no deadhead travel. The return escort trip is included, as are the
+deadhead limo and taxi trips.
+
+The scripts reads in csv files adapted from Mr. Gosling's Excel files,
+and creates a highway-assignment ready OMX matrix file for each time-of-day
+interval.
+
+Assumes that no air passengers use HOT lanes (probably not exactly true
+in certain future year scenarios, but the assumption is made here as a
+simplification).  Simple linear interpolations are used to estimate vehicle
+demand in years other than 2007 and 2035, including 2015, 2020, 2025, 2030,
+and 2040.  The 2007 table is used for years 2000, 2005, and 2010.
+
+Transit travel to the airports is not included in these vehicle trip tables.
+
+Input:
+    Year-, access/egress-, and airport-specific database file with 90 columns
+    of data for each TAZ.  There are 18 columns for each time-of-day interval
+    as follows:
             (1)   Escort, drive alone
             (2)   Escort, shared ride 2
             (3)   Escort, shared ride 3+
@@ -52,62 +70,154 @@ NOTES FROM BuildAirPaxMatrices.job
             (18)  Charter bus, shared ride 3+
 
 
- Output: (A) Five time-of-day-specific tables, each containing origin/destination vehicle matrices for the following modes:
+ Output:
+ Five time-of-day-specific tables, each containing origin/destination vehicle
+ matrices for the following modes:
            (1) drive alone (DA)
            (2) shared ride 2 (SR2)
            (3) shared ride 3+ (SR3)
-           (4) drive alone and willing to pay a value toll (DATOLL)
-           (5) shared ride 2 and willing to pay a value toll (SR2TOLL)
-           (6) shared ride 3+ and willing to pay a value toll (SR3TOLL)
-
 """
 
 
-# from contextlib import contextmanager as _context
 import os as _os
+import numpy as _numpy
+import openmatrix as _omx
 import pandas as _pandas
-from tm2py.core.component import Component as _Component
 
-# import tm2py.core.tools as _tools
-# import tm2py.core.emme as _emme_tools
+from tm2py.core.component import Component as _Component, Controller as _Controller
 
 
 _join, _dir = _os.path.join, _os.path.dirname
 
 
 class AirPassenger(_Component):
-    """docstring for component"""
+    """Builds the airport trip matrices."""
 
-    def __init__(self, controller):
+    def __init__(self, controller: _Controller, root_dir: str = None):
+        """Build the airport trip matrices.
+
+        Args:
+            controller: parent Controller object
+            root_dir (str): root directory containing Emme project, demand matrices
+        """
         super().__init__(controller)
-        self._parameter = None
+        if root_dir is None:
+            self._root_dir = _os.getcwd()
+        else:
+            self._root_dir = root_dir
+        self._periods = ["EA", "AM", "MD", "PM", "EV"]
+        self._access_modes = ["ES", "PK", "RN", "TX", "LI"]
+        self._s3_access_modes = self._access_modes + ["VN", "HT", "CH"]
+        self._assign_classes = ["DA", "S2", "S3"]
 
     def run(self):
-        """docstring for component run"""
-        year = self._config.model.year
-        self._load_demand()
-        self._sum_demand()
-        self._interpolate()
-        self._export_result()
+        """Build the airport trip matrices"""
+        input_demand = self._load_demand()
+        aggr_demand = self._aggregate_demand(input_demand)
+        demand = self._interpolate(aggr_demand)
+        self._export_result(demand)
 
     def _load_demand(self):
-        years = ["2007", "2035"]
-        airports = ["SFO", "OAK", "SJC"]
-        to_from = ["to", "from"]
-        # TODO: load each .csv into pandas data frame and return dataframe(s)
-        # nonres\@token_year@_@token_toFrom@@token_airport@.csv
-        # nonres\@token_year@_@token_toFrom@@token_airport@_seq.csv
+        """Loads demand from the CSV files into pandas dataframe."""
 
-    def _sum_demand(self, dataframes):
-        # TODO: aggregate demand columns from data frames into the
-        #       assignable classes for each year (2007 and 2035)
-        pass
+        def rename_columns(name):
+            if name in ["ORIG", "DEST"]:
+                return name
+            return f"{name}_{year}"
 
-    def _interpolate(self):
-        # TODO: interpolate between the defined years
-        pass
-        # token_scale = '(%MODEL_YEAR% - 2007)/(2035 - %MODEL_YEAR%)'
+        input_demand = []
+        for year in ["2007", "2035"]:
+            input_dataframes = []
+            for airport in ["SFO", "OAK", "SJC"]:
+                for direction in ["to", "from"]:
+                    file_name = f"{year}_{direction}{airport}.csv"
+                    file_path = _join(self._root_dir, "nonres", file_name)
+                    input_df = _pandas.read_csv(file_path)
+                    input_df.rename(columns=rename_columns, inplace=True)
+                    input_dataframes.append(input_df)
+            data = _pandas.concat(input_dataframes)
+            input_demand.append(data)
+        demand = _pandas.merge(
+            input_demand[0], input_demand[1], how="outer", on=["ORIG", "DEST"]
+        )
+        grouped = demand.groupby(["ORIG", "DEST"]).sum()
+        return grouped
 
-    def _export_result(self):
-        # TODO: resulting demand for the model year
-        pass
+    def _aggregate_demand(self, input_demand):
+        """Aggregate demand into the assignable classes for each year (2007 and 2035)."""
+        aggr_demand = _pandas.DataFrame()
+        for year in self._ref_years:
+            for period in self._periods:
+                for assign in self._assign_classes:
+                    if assign == "S3":
+                        acces_modes = self._s3_access_modes
+                    else:
+                        acces_modes = self._access_modes
+                    data = input_demand[
+                        [f"{period}_{access}_{assign}_{year}" for access in acces_modes]
+                    ]
+                    aggr_demand[f"{period}_{assign}_{year}"] = data.sum(axis=1)
+        return aggr_demand
+
+    def _interpolate(self, aggr_demand):
+        """Interpolate for the model year assuming linear growth between the reference years."""
+        year = str(self.config.model.year)
+        if year in self._ref_years:
+            # no interpolation is needed
+            columns = [c for c in aggr_demand.columns if c.endswith(year)]
+            demand = aggr_demand[columns].copy()
+
+            def rename_columns(name):
+                return name.replace(f"_{year}", "")
+
+            demand.rename(columns=rename_columns, inplace=True)
+            return demand
+
+        # In the cube .job script, the formula is:
+        # token_scale = (%MODEL_YEAR% - 2007)/(2035 - %MODEL_YEAR%)
+        # and it should be: token_scale = (%MODEL_YEAR% - 2007)/(2035 - 2007)
+        # scale = float((int(year) - 2007)) / (2035 - int(year))
+        scale = (int(year) - 2007) / (2035 - 2007)
+        demand = _pandas.DataFrame()
+        for period in self._periods:
+            for assign in self._assign_classes:
+                name = f"{period}_{assign}"
+                demand[name] = (1 - scale) * aggr_demand[
+                    f"{name}_2007"
+                ] + scale * aggr_demand[f"{name}_2035"]
+        return demand
+
+    def _export_result(self, demand):
+        """Export resulting model year demand to OMX files by period."""
+        # dropping ORIG and DEST index for calculation of numpy array index
+        demand = demand.reset_index()
+        # get all used Zone IDs to produce index and zone mapping in OMX file
+        zone_ids = sorted(set(demand["ORIG"]).union(set(demand["DEST"])))
+        num_zones = len(zone_ids)
+        zone_map = dict((z, i) for i, z in enumerate(zone_ids))
+        # calculate index of entries in numpy array list
+        demand["INDEX"] = demand.apply(
+            lambda r: zone_map[r["ORIG"]] * len(zone_ids) + zone_map[r["DEST"]], axis=1
+        )
+        for period in self._periods:
+            file_path = _join(
+                self._root_dir,
+                "demand_matrices",
+                "highway",
+                "air_passenger",
+                f"tripsAirPax{period}.omx",
+            )
+            omx_file = _omx.open_file(file_path, "w")
+            try:
+                omx_file.create_mapping("zone_number", zone_ids)
+                for name in self._assign_classes:
+                    array = _numpy.zeros(shape=(num_zones, num_zones))
+                    # Insert values at the calculated indices in array
+                    _numpy.put(
+                        array,
+                        demand["INDEX"].to_numpy(),
+                        demand[f"{period}_{name}"].to_numpy(),
+                    )
+                    omx_file.create_matrix(name, obj=array)
+            finally:
+                omx_file.close()
