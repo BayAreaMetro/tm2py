a36699facca149cea120f5a82cfa197ac478fd60
Kevin Bragg <kevin@inrosoftware.com>
2021-10-12 15:53:27 -0400

    Adding some very basic logging functions, log with timestamp and log start and end of method / context statement

--- tm2py/core/logging.py
+++ tm2py/core/logging.py
@@ -1,6 +1,9 @@
 """Logging module
 
 """
+from contextlib import contextmanager as _context
+from datetime import datetime
+import functools
 
 
 class Logger:
@@ -9,6 +12,7 @@ class Logger:
     def __init__(self, controller):
         super().__init__()
         self._controller = controller
+        self._indentation = 0
 
     @property
     def controller(self):
@@ -21,6 +25,49 @@ class Logger:
         return self.controller.config
 
     @staticmethod
-    def log(text):
+    def log(msg, level=1):
         """Placeholder logging method"""
-        print(text)
+        print(msg)
+
+    def log_time(self, msg, level=1):
+        """Log message with timestamp"""
+        timestamp = datetime.now().strftime("%d-%b-%Y (%H:%M:%S)")
+        self.log(f"{timestamp}: {msg}", level)
+
+    def log_start(self, msg, level=1):
+        """Log message with timestamp"""
+        indent = "  " * self._indentation
+        self.log_time(f"{indent}Start {msg}", level)
+        self._indentation += 1
+
+    def log_end(self, msg, level=1):
+        self._indentation -= 1
+        indent = "  " * self._indentation
+        self.log_time(f"{indent}End {msg}", level)
+
+    @_context
+    def log_start_end(self, msg, level=1):
+        self.log_start(msg, level)
+        yield
+        self.log_end(msg, level)
+
+class LogStartEnd:
+    """Log the start and end time with optional message.
+
+    Can be used as a Component method decorator or in with statement
+    """
+
+    def __init__(self, msg=None, level=1):
+        self.msg = msg
+        self.level = level
+
+    def __call__(self, func):
+        @functools.wraps(func)
+        def wrapper(obj, *args, **kwargs):
+            msg = self.msg or obj.__class__.__name__ + " " + func.__name__
+            obj.logger.log_start(msg, self.level)
+            value = func(obj, *args, **kwargs)
+            obj.logger.log_end(msg, self.level)
+            return value
+
+        return wrapper
