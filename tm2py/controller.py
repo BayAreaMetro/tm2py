d449d18b141fc50d8fc5afde18aa087ced4e85b0
Kevin Bragg <kevin@inrosoftware.com>
2021-12-09 14:02:57 -0500

    Basic RunController functionality

--- tm2py/controller.py
+++ tm2py/controller.py
@@ -6,17 +6,28 @@ import itertools
 import os
 
 from .config import Configuration
+from .emme.manager import EmmeManager
 from .logger import Logger
-from .emme.cache import EmmeProjectCache
-from .compute import parse_num_processors
-import components
+from . import components
+
+from typing import Union, List
+
+# pylint: disable=too-many-instance-attributes
+
 
 class RunController:
-    """docstring for Controller class"""
+    """Main operational interface for model runs.
 
-    def __init__(self, run_config_file: str = None):
-        if not run_config_file:
-            config_file = os.path.join(os.getcwd(), "config.toml")
+    Provide one or more config files in TOML (*.toml) format, and a run directory.
+    If the run directory is not provided the root directory of the first config_file is used.
+    """
+
+    def __init__(self, config_file: Union[List[str], str] = None, run_dir: str = None):
+        if not isinstance(config_file, list):
+            config_file = [config_file]
+        self._run_dir = run_dir
+        if self._run_dir is None:
+            self._run_dir = os.path.dirname(config_file[0])
 
         self.config = Configuration(config_file)
         self.logger = Logger(self)
@@ -24,40 +35,29 @@ class RunController:
         self.top_sheet = None
         self.trace = None
 
-        self._emme_bank = None
-        self._emme_manager = EmmeProjectCache()
+        self._emme_manager = EmmeManager()
         self._emme_manager.project(self.run_dir)
-        self._emmebank_dir = os.path.join(
-            self.run_dir, "mtc_emme_transit", "Database", "emmebank"
-        )
 
-        self.num_processors = parse_num_processors(self.config.num_processors)
-
+        self.component_map = {
+            "highway": components.network.highway.highway_assign.HighwayAssignment(
+                self
+            ),
+        }
         self.completed_components = []
-
-        self._queued_components = []
         self._iteration = None
         self._component = None
+        self._queued_components = []
         self._queue_components()
-
-
-    @property
-    def _modeller(self):
-        return self._emme_manager.modeller
 
     @property
     def run_dir(self):
-        "shortcut to access run directory"
-        return self.config.run_dir
-
-    @property
-    def emme_bank(self):
-        return self._emme_bank
+        """Shortcut to access run directory"""
+        return self._run_dir
 
     @property
     def queued_components(self):
         """List of all component objects"""
-        return self._queued_components
+        return list(self._queued_components)
 
     @property
     def iteration(self):
@@ -71,49 +71,47 @@ class RunController:
 
     def run(self):
         """Main interface to run model"""
-
         self.validate_inputs()
-        for i, c in self.queued_components():
-            self._iteration = i
-            self._component = c
-            components.c.run()
+        for iteration, component in self._queued_components:
+            self._iteration = iteration
+            self._component = component
+            component.run()
+            self.completed_components.append((iteration, component))
 
     def _queue_components(self):
         """[summary]"""
-        if self.config.start_iteration == 0:
+        if self.config.run.start_iteration == 0:
             self._queued_components += [
                 (0, self.component_map[c_name])
-                for c_name in self.config.initialize_components
+                for c_name in self.config.run.initial_components
             ]
         self._queued_components += list(
             itertools.product(
                 range(
-                    max(1, self.config.start_iteration),
-                    self.config.run.global_iterations + 1,
+                    max(1, self.config.run.start_iteration),
+                    self.config.run.end_iteration + 1,
                 ),
-                self.config.global_iteration_components,
+                self.config.run.global_iteration_components,
             )
         )
 
-        if self.config.start_component:
+        if self.config.run.start_component:
             start_index = [
                 idx
                 for idx, c in enumerate(self._queued_components)
-                if self.config.start_component in c[1]
+                if self.config.run.start_component in c[1]
             ][0]
             self._queued_components = self._queued_components[start_index:]
 
     def validate_inputs(self):
         """Validate input state prior to run"""
-        for component in self.components.values():
+        for _, component in self._queued_components:
             component.validate_inputs()
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Main: run MTC TM2PY")
-    parser.add_argument("-a", "--argument", help=r"An argument")
+    parser.add_argument("-s", "--scenario", help=r"Scenario config file path")
     args = parser.parse_args()
-
-    controller = RunController()
-    controller.argument = args.argument
+    controller = RunController(args.scenario)
     controller.run()
