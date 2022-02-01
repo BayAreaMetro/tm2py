ae93fdba90fc386d7441dbfe121144be850e0c8b
Kevin Bragg <kevin@inrosoftware.com>
2022-02-01 11:06:16 -0500

    Fixed / updated tests for config

--- tests/test_config.py
+++ tests/test_config.py
@@ -6,24 +6,26 @@ EXAMPLE_DIR = os.path.join(
     os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "examples"
 )
 TEST_CONFIG = os.path.join(EXAMPLE_DIR, "run_config.toml")
+MODEL_CONFIG = os.path.join(EXAMPLE_DIR, "model_config.toml")
 
 
 def test_config_read():
     """Configuration should load parameters to the correct namespaces."""
     from tm2py.config import Configuration
 
-    my_config = Configuration(TEST_CONFIG)
+    my_config = Configuration([TEST_CONFIG, MODEL_CONFIG])
 
     assert my_config.run.start_iteration == 0
-    assert my_config.run.end_iteration == 1
+    assert my_config.run.end_iteration == 2
     assert my_config.scenario.year == 2015
     assert my_config.run.initial_components == [
         "create_tod_scenarios",
         "active_modes",
         "air_passenger",
-        "highway_maz_maz",
+        "prepare_network_highway",
         "highway",
-        "transit",
+        "highway_maz_skim",
+        "transit"
     ]
     assert len(my_config.time_periods) == 5
     assert my_config.highway.classes[0].description == "drive alone"
