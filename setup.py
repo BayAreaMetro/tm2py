856a9e5440489ef77365cf9bd8a7c4b30a2d4cf7
Elizabeth Sall <easall@gmail.com>
2022-04-20 13:03:46 -0700

    take docs requirements out of setup

--- setup.py
+++ setup.py
@@ -25,10 +25,6 @@ with open("dev-requirements.txt") as f:
     dev_requirements = f.readlines()
 install_requires_dev = [r.strip() for r in dev_requirements]
 
-with open(os.path.join("docs", "requirements.txt")) as f:
-    doc_requirements = f.readlines()
-install_requires_doc = [r.strip() for r in doc_requirements]
-
 # While version is in active development, install both development and base requirements.
 major_version_number = int(version.split(".")[0])
 if major_version_number < 1:
