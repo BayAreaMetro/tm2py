4c04a1d0551564202f3bf7adc4e8d256db592c3d
Kevin Bragg <kevin@inrosoftware.com>
2022-04-26 16:36:51 -0400

    Adding placeholder (partial implementation) for calling household CT-RAMP model

---
+++ tm2py/components/demand/household.py
@@ -0,0 +1,61 @@
+"""Placeholder docstring for CT-RAMP related components for household residents' model
+"""
+
+import shutil as _shutil
+
+from tm2py.components.component import Component
+from tm2py.tools import run_process
+from tm2py.logger import LogStartEnd
+
+
+class HouseholdModel(Component):
+    """Run household resident model"""
+
+    def __init__(self, controller):
+        super().__init__(controller)
+
+    @LogStartEnd()
+    def run(self):
+        self._start_household_manager()
+        self._start_matrix_manager()
+        self._run_resident_model()
+        self._stop_java()
+
+    @staticmethod
+    def _start_household_manager():
+        commands = [
+            "CALL CTRAMP\\runtime\\CTRampEnv.bat",
+            "set PATH=%CD%\\CTRAMP\runtime;C:\\Windows\\System32;%JAVA_PATH%\bin;"
+            "%TPP_PATH%;%PYTHON_PATH%;%PYTHON_PATH%\\condabin;%PYTHON_PATH%\\envs",
+            'CALL CTRAMP\\runtime\\runHhMgr.cmd "%JAVA_PATH%" "%HOST_IP_ADDRESS%"',
+        ]
+        run_process(commands, name="start_household_manager")
+
+    @staticmethod
+    def _start_matrix_manager():
+        commands = [
+            "CALL CTRAMP\\runtime\\CTRampEnv.bat",
+            "set PATH=%CD%\\CTRAMP\runtime;C:\\Windows\\System32;%JAVA_PATH%\bin;"
+            "%TPP_PATH%;%PYTHON_PATH%;%PYTHON_PATH%\\condabin;%PYTHON_PATH%\\envs",
+            'CALL CTRAMP\runtime\runMtxMgr.cmd %HOST_IP_ADDRESS% "%JAVA_PATH%"',
+        ]
+        run_process(commands, name="start_matrix_manager")
+
+    def _run_resident_model(self):
+        sample_rate_iteration = {1: 0.3, 2: 0.5, 3: 1, 4: 0.02, 5: 0.02}
+        iteration = self.controller.iteration
+        sample_rate = sample_rate_iteration[iteration]
+        _shutil.copyfile("CTRAMP\\runtime\\mtctm2.properties", "mtctm2.properties")
+        commands = [
+            "CALL CTRAMP\\runtime\\CTRampEnv.bat",
+            "set PATH=%CD%\\CTRAMP\runtime;C:\\Windows\\System32;%JAVA_PATH%\bin;"
+            "%TPP_PATH%;%PYTHON_PATH%;%PYTHON_PATH%\\condabin;%PYTHON_PATH%\\envs",
+            'CALL CTRAMP\runtime\runMTCTM2ABM.cmd {sample_rate} {iteration} "%JAVA_PATH%"'.format(
+                sample_rate=sample_rate, iteration=iteration
+            ),
+        ]
+        run_process(commands, name="run_resident_model")
+
+    @staticmethod
+    def _stop_java():
+        run_process(['taskkill /im "java.exe" /F'])
