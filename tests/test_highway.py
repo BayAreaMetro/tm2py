d82269e332f4d0fbce4427bd9cd81bfe7aa6f6bc
Sijia Wang <wangsijia0628@gmail.com>
2022-12-21 19:28:50 -0500

    separate out maz tests

--- tests/test_highway.py
+++ tests/test_highway.py
@@ -6,55 +6,27 @@ import pytest
 from conftest import inro_context
 from tools import assert_csv_equal, diff_omx
 
+from tm2py.examples import get_example
 
-@pytest.mark.skipif(inro_context != "inro", reason="requires full inro context")
-def test_prepare_highway_network(examples_dir):
+
+def test_highway(inro_context, examples_dir, root_dir):
     "Tests that prepare highway network component can be run."
     from tools import test_component
 
-    my_run = test_component(examples_dir, "prepare_network_highway")
-    my_run.run_next()
+    get_example(example_name="UnionCity", root_dir=root_dir)
+
+    my_run = test_component(examples_dir, ["prepare_network_highway", "highway"])
+
+    if inro_context != "inro":
+        return
+    my_run.run()
 
     # TODO write assert
 
 
-@pytest.mark.skipif(inro_context != "inro", reason="requires full inro context")
-def test_highway_assign(examples_dir):
-    "Tests that highway network assignment component can be run."
-    from tools import test_component
-
-    my_run = test_component(examples_dir, "highway")
-    my_run.run_next()
-
-    # TODO write assert
-
-
-@pytest.mark.skipif(inro_context != "inro", reason="requires full inro context")
-def test_highway_maz_assign(examples_dir):
-    "Tests that highway MAZ network assignment component can be run."
-    from tools import test_component
-
-    my_run = test_component(examples_dir, "highway_maz_assign")
-    my_run.run_next()
-
-    # TODO write assert
-
-
-@pytest.mark.skipif(inro_context != "inro", reason="requires full inro context")
-def test_highway_maz_skim(examples_dir):
-    "Tests that highway MAZ network skimming component can be run."
-    from tools import test_component
-
-    my_run = test_component(examples_dir, "highway_maz_skim")
-    my_run.run_next()
-
-    # TODO write assert
-
-
-@pytest.mark.skipif(inro_context != "inro", reason="requires full inro context")
-def test_highway_skims(union_city):
+def test_highway_skims(examples_dir):
     """Test that the OMX highway skims match the reference."""
-    run_dir = union_city.run_dir
+    run_dir = os.path.join(examples_dir, "UnionCity")
 
     ref_dir_hwy_skims = os.path.join(run_dir, "ref_skim_matrices", "highway")
     ref_skim_files = glob.glob(os.path.join(ref_dir_hwy_skims, "*.omx"))
@@ -81,17 +53,3 @@ def test_highway_skims(union_city):
 
     assert len(missing_skims) == 0, f"Missing skims: {missing_skims}"
     assert len(different_skims) == 0, f"Different skims: {different_skims}"
-
-
-@pytest.mark.skipif(inro_context != "inro", reason="requires full inro context")
-def test_maz_da_skims(union_city):
-    """Test that the DA MAZ skims match the reference."""
-    run_dir = union_city.run_dir
-
-    ref_dir_hwy_skims = os.path.join(run_dir, "ref_skim_matrices", "highway")
-    run_dir_hwy_skims = os.path.join(run_dir, "skim_matrices", "highway")
-
-    ref_csv = os.path.join(ref_dir_hwy_skims, "HWYSKIM_MAZMAZ_DA.csv")
-    run_csv = os.path.join(run_dir_hwy_skims, "HWYSKIM_MAZMAZ_DA.csv")
-
-    return assert_csv_equal(ref_csv, run_csv)
