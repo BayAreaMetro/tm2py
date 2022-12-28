1147aff50d3abc124c4a03a173e47388371e2606
David Ory <david.ory@gmail.com>
2022-12-28 11:13:33 -0500

    Move to dev environment, sketch out observed toml, add notebook to help
    Cleaned out __init__.py to avoid inro dependency on import bug
    skip tests

--- tm2py/acceptance.py
+++ tm2py/acceptance.py
@@ -1,50 +1,66 @@
 """Methods to create Acceptance Criteria summaries from a tm2py model run."""
 
-
-from tm2py.config import Configuration
 from typing import Union, List
 
+import os
 import geopandas as gpd
 import pandas as pd
 import toml
 
+
 class Acceptance:
 
-    scenario_config: Configuration
+    def _load_configs(self):
+         
+        with open(self.scenario_file, "r", encoding="utf-8") as toml_file:
+            self.scenario_dict = toml.load(toml_file)
+         
+        with open(self.observed_file, "r", encoding="utf-8") as toml_file:
+            self.observed_dict = toml.load(toml_file)
+            
+        return
+
+    def __init__(self, scenario_file: str, observed_file: str) -> None:
+        self.scenario_file = scenario_file
+        self.observed_file = observed_file
+        self._load_configs()
+
+    scenario_dict: dict
     observed_dict: dict
+
+    reduced_on_board_survey_df: pd.DataFrame
 
     tableau_projection = 4326
 
     road_network_gdf = gpd.GeoDataFrame(
         {
-            "model_link_id": pd.Series(dtype = "int"),
-            "pems_station_id": pd.Series(dtype = "int"),
+            "model_link_id": pd.Series(dtype="int"),
+            "pems_station_id": pd.Series(dtype="int"),
             "time_period": pd.Series(dtype="str"),
             "observed_flow": pd.Series(dtype="float"),
             "simulated_flow": pd.Series(dtype="float"),
             "odot_flow_category": pd.Series(dtype="int"),
             "odot_maximum_error": pd.Series(dtype="float"),
-            "line_string": pd.Series(dtype="str") 
-
+            "line_string": pd.Series(dtype="str"),
         }
     )
 
     transit_network_gdf = gpd.GeoDataFrame(
         {
-            "model_link_id": pd.Series(dtype = "int"),
+            "model_link_id": pd.Series(dtype="int"),
             "operator": pd.Series(dtype="str"),
             "route": pd.Series(dtype="str"),
             "direction": pd.Series(dtype="int"),
             "time_period": pd.Series(dtype="str"),
             "observed_passengers": pd.Series(dtype="float"),
             "simulated_passengers": pd.Series(dtype="float"),
-            "line_string": pd.Series(dtype="str"), 
+            "line_string": pd.Series(dtype="str"),
         }
     )
 
     compare_gdf = gpd.GeoDataFrame(
         {
-            "criteria_number": pd.Series(dtype = "int"),
+            "criteria_number": pd.Series(dtype="int"),
             "criteria_name": pd.Series(dtype="str"),
             "dimension_01_name": pd.Series(dtype="str"),
             "dimension_01_value": pd.Series(dtype="float"),
@@ -55,42 +71,44 @@ class Acceptance:
             "observed_outcome": pd.Series(dtype="float"),
             "simulated_outcome": pd.Series(dtype="float"),
             "acceptance_threshold": pd.Series(dtype="float"),
-            "point_string": pd.Series(dtype="str"), 
+            "point_string": pd.Series(dtype="str"),
         }
     )
-    
-    def make_acceptance(scenario_file: str, observed_config_file: str):
 
-        scenario_config = Configuration.load_toml(path = scenario_file)
+    def make_acceptance(self):
 
-        with open(observed_config_file, "r", encoding="utf-8") as toml_file:
-            observed_dict = toml.load(toml_file)
+        self._load_configs()
 
         # _validate() method to validate the scenario and observed data to fail fast
         # _make_roadway_network_comparisons() method to build roadway network comparisons
-        # _make_transit_network_comparisons() 
+        # _make_transit_network_comparisons()
         # _make_other_comparisons()
 
         return
 
     def _validate():
 
-        #_validate_scenario()
-        #_validate_observed()
+        # _validate_scenario()
+        # _validate_observed()
 
         return
 
     def _validate_scenario():
 
         # is the model run complete?
-        # are the key files present? 
+        # are the key files present?
 
         return
 
-    def _validate_observed():
+    def _validate_observed(self):
 
-        # are all the key files present? 
+        # are all the key files present?
         # do time period names align with the scenario config?
+
+        if self.reduced_on_board_survey_df is None:
+            self._reduce_on_board_survey_df()
+
+
 
         return
 
@@ -121,10 +139,10 @@ class Acceptance:
 
         return
 
-    def _make_roadway_network_comparisons(): 
+    def _make_roadway_network_comparisons():
 
-        # placeholder 
-        # _write_roadway_network()  
+        # placeholder
+        # _write_roadway_network()
 
         return
 
@@ -135,5 +153,20 @@ class Acceptance:
     def _make_other_comparisons():
 
         # separate method for each one
+
+        return
+
+    def _reduce_on_board_survey(self):
+
+        file_root = self.observed_dict["folder_root"]
+        in_file = self.observed_dict["transit"]["on_board_survey_file"]
+        out_file = self.observed_dict["transit"]["reduced_summaries_file"]
+
+        if os.path.isfile(out_file):
+            self.reduced_on_board_survey_df = pd.read_csv(os.path.join(file_root, out_file))
+        else:
+            in_df = pd.read_csv(os.path.join(file_root, in_file))
+            out_df = in_df.groupby(["operator", "route", "time_period"])["boarding_weight"].sum().reset_index()
+            out_df.to_csv(os.path.join(file_root, out_file))
 
         return
