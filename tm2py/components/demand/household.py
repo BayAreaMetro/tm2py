8429ab143ddfce21d4654da60fa5f3ce7a59c2c3
Arash <39972246+arashasadabadi@users.noreply.github.com>
2023-10-02 11:50:05 -0700

    Update household.py

--- tm2py/components/demand/household.py
+++ tm2py/components/demand/household.py
@@ -7,6 +7,7 @@ import openmatrix as omx
 from tm2py.components.component import Component
 from tm2py.logger import LogStartEnd
 from tm2py.tools import run_process
+from tm2py.components.demand.prepare_demand import PrepareHighwayDemand
 
 
 class HouseholdModel(Component):
@@ -31,7 +32,12 @@ class HouseholdModel(Component):
         self._start_matrix_manager()
         self._run_resident_model()
         self._stop_java()
-        self._consolidate_demand_for_assign()
+        #self._consolidate_demand_for_assign()
+        self._prepare_demand_for_assignment()
+
+    def _prepare_demand_for_assignment(self):
+        prep_demand = PrepareHighwayDemand(self.controller)
+        prep_demand.prepare_household_demand()
 
     def _start_household_manager(self):
         commands = [
@@ -54,7 +60,7 @@ class HouseholdModel(Component):
         run_process(commands, name="start_matrix_manager")
 
     def _run_resident_model(self):
-        sample_rate_iteration = {1: 0.3, 2: 0.5, 3: 1, 4: 0.02, 5: 0.02}
+        sample_rate_iteration = {1: 0.05, 2: 0.5, 3: 1, 4: 0.02, 5: 0.02}
         iteration = self.controller.iteration
         sample_rate = sample_rate_iteration[iteration]
         _shutil.copyfile("CTRAMP\\runtime\\mtctm2.properties", "mtctm2.properties")
@@ -140,33 +146,62 @@ class HouseholdModel(Component):
                 _shutil.copyfile(input_path, output_path)
 
         # transit TAP
+        #for period in time_period_names:
+        #    for set in ["set1", "set2", "set3"]:
+        #        output_path = (
+        #            self.controller.get_abs_path(self.config.transit_demand_file)
+        #            .__str__()
+        #            .format(period=period, iter=self.controller.iteration, set=set)
+        #        )
+        #        output_omx = omx.open_file(output_path, "w")
+        #        for mode_agg in self.config.mode_agg:
+        #            if mode_agg.name != "transit":
+        #                continue
+        #            for mode in mode_agg.modes:
+        #                input_path = (
+        #                    self.controller.get_abs_path(
+        #                        self.config.transit_tap_ctramp_output_file
+        #                    )
+        #                    .__str__()
+        #                    .format(
+        #                        period=period,
+        #                        mode_agg=mode_agg.name,
+        #                        mode=mode,
+        #                        set=set,
+        #                    )
+        #                )
+        #                input_omx = omx.open_file(input_path, "r")
+        #                core_name = mode + "_TRN_" + set + "_" + period.upper()
+        #                output_omx[core_name] = input_omx[core_name][:, :]
+        #                input_omx.close()
+#
+        #        output_omx.close()
+        # transit TAZ
         for period in time_period_names:
-            for set in ["set1", "set2", "set3"]:
-                output_path = (
-                    self.controller.get_abs_path(self.config.transit_demand_file)
-                    .__str__()
-                    .format(period=period, iter=self.controller.iteration, set=set)
-                )
-                output_omx = omx.open_file(output_path, "w")
-                for mode_agg in self.config.mode_agg:
-                    if mode_agg.name != "transit":
-                        continue
-                    for mode in mode_agg.modes:
-                        input_path = (
-                            self.controller.get_abs_path(
-                                self.config.transit_tap_ctramp_output_file
-                            )
-                            .__str__()
-                            .format(
-                                period=period,
-                                mode_agg=mode_agg.name,
-                                mode=mode,
-                                set=set,
-                            )
+            output_path = (
+                self.controller.get_abs_path(self.config.transit_demand_file)
+                .__str__()
+                .format(period=period, iter=self.controller.iteration)
+            )
+            output_omx = omx.open_file(output_path, "w")
+            for mode_agg in self.config.mode_agg:
+                if mode_agg.name != "transit":
+                    continue
+                for mode in mode_agg.modes:
+                    input_path = (
+                        self.controller.get_abs_path(
+                            self.config.transit_taz_ctramp_output_file
                         )
-                        input_omx = omx.open_file(input_path, "r")
-                        core_name = mode + "_TRN_" + set + "_" + period.upper()
-                        output_omx[core_name] = input_omx[core_name][:, :]
-                        input_omx.close()
+                        .__str__()
+                        .format(
+                            period=period,
+                            mode_agg=mode_agg.name,
+                            mode=mode,
+                        )
+                    )
+                    input_omx = omx.open_file(input_path, "r")
+                    core_name = mode + "_TRN_"  + period.upper()
+                    output_omx[core_name] = input_omx[core_name][:, :]
+                    input_omx.close()
 
-                output_omx.close()
+            output_omx.close()
