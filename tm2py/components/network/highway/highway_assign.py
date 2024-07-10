99325468f8ac3862b0aadee7b056461a9af39ec4
Sijia Wang <wangsijia0628@gmail.com>
2024-07-10 10:37:19 -0400

    revert temp change

--- tm2py/components/network/highway/highway_assign.py
+++ tm2py/components/network/highway/highway_assign.py
@@ -152,7 +152,7 @@ class HighwayAssignment(Component):
             demand.run()
         else:
             self.highway_emmebank.zero_matrix
-        for time in ["AM"]: #self.time_period_names:
+        for time in self.time_period_names:
             scenario = self.highway_emmebank.scenario(time)
             with self._setup(scenario, time):
                 iteration = self.controller.iteration
